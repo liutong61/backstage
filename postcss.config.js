18fc09d09f5a5611afb6bd4b7420f7629c9bc654
haiquan.huang <haiquan.huang@haiquanhuangs-MacBook-Pro.local>
2019-08-13 16:26:12 +0800

    提交

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,15 @@
+const webpack = require('webpack')
 module.exports = {
   plugins: {
     autoprefixer: {}
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+        "windows.jQuery": "jquery"
+      })
+    ]
   }
 }
